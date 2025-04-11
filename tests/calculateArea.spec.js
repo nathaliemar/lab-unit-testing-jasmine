8cf65c70ffd3bf81d32e931afd758e47d5910542
nathaliemar <n.marten.95@gmail.com>
2025-04-11 17:10:21 +0200

    Solved lab

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,19 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+  describe("Function - calculateArea", () => {
+    it("should be defined", () => {
+      expect(calculateArea).toBeDefined();
+    });
+    it("should take two numbers as arguments", () => {
+      expect(calculateArea.length).toBe(2);
+    });
+    it("should return the product of the two arguments", () => {
+      expect(calculateArea(3, 3)).toEqual(9);
+      expect(calculateArea(7, 2)).toEqual(14);
+      expect(calculateArea(3, 0)).toEqual(0);
+    });
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(calculateArea(3)).toEqual(undefined);
+      expect(calculateArea()).toEqual(undefined);
+    });
+  });
+});
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,18 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+  describe("Function - divide", () => {
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
+    it("should take two numbers as arguments", () => {
+      expect(divide.length).toBe(2);
+    });
+    it("should return the division of two numbers", () => {
+      expect(divide(6, 2)).toEqual(3);
+      expect(divide(100, 50)).toEqual(2);
+    });
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(divide(6)).toEqual(undefined);
+      expect(divide(8)).toEqual(undefined);
+    });
+  });
+});
